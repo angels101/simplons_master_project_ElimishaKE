f947e590ddbb19a19c8797a16996297f491a1307
angels101 <angelscodex101@gmail.com>
2021-03-22 11:23:16 +0300

    Newsletter

---
+++ news/urls.py
@@ -0,0 +1,19 @@
+
+from news.models import Article
+from django.urls import path
+from .import views
+from django.conf.urls .static import static
+from django.conf import settings
+
+
+urlpatterns = [
+        path('', views.welcome, name= 'welcome'),
+        path('news_today/', views.news_today, name='newsToday'),
+        path('archives/<str:past_date>/', views.past_days_news, name = 'pastNews'),
+        path('search/', views.search_results, name='search_results'),
+        path('article/(\d+)', views.article, name='article')
+]
+
+if settings.DEBUG:
+        #urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+        urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
