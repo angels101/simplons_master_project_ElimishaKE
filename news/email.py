52708cf143644373fa75fd6667e2dd7c4de96e1a
angels101 <angelscodex101@gmail.com>
2021-04-17 10:32:59 +0300

    email py,news email txt and newsemail text

--- news/email.py
+++ news/email.py
@@ -2,7 +2,7 @@ from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
 
 
-def send_welcome_email(name,,reciever):
+def send_welcome_email(name, reciever):
     #Creating message subject and sender
 
     subject = 'Welcome to the ElimishaTribune Newsletter'
@@ -14,7 +14,7 @@ def send_welcome_email(name,,reciever):
     html_content = render_to_string('email/newsemail.html',{"name": name})
 
     msg = EmailMultiAlternatives(subject, text_content, sender,[reciever])
-    msg.attach_alternative(html_content.'text/html')
+    msg.attach_alternative(html_content, 'text/html')
     msg.send()
 
     
